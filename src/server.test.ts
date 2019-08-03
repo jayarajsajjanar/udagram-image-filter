2df6710e6f7c3eacd606ccebd4da31f085481d9c
Steve Carrupt <steve@codefactoring.com>
2019-08-03 23:49:10 +1000

    FilteredImage endpoint returns a image with the filter applied
    - Shutdown express server at the end of the test

--- src/server.test.ts
+++ src/server.test.ts
@@ -2,9 +2,16 @@ import request from 'supertest'
 const server = require('./server')
 
 describe('Image filter api', () => {
-    it('returns OK when calling filteredimage endpoint', async () => {
+    it('returns the image from the url with the filter applied', async () => {
         await request(server)
-            .get('/filteredimage')
+            .get('/filteredimage?image_url=https://timedotcom.files.wordpress.com/2019/03/kitten-report.jpg')
             .expect(200)
+            .then(response => {
+                expect(response.header['content-type']).toBe('image/jpeg')
+            })
+    })
+
+    afterAll(() => {
+        server.close()
     })
 })
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -31,23 +31,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
   app.get("/filteredimage", async (req, res) => {
-      res.status(200).send()
+    const {image_url} = req.query
+    const img = await filterImageFromURL(image_url)
+    res.sendFile(img)
   });
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  const server = app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 
-  module.exports = app
+  module.exports = server
 })();
 
 
