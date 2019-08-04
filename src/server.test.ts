10f2807537c66b73e4b34ef2e6d207a5451c982f
Steve Carrupt <steve@codefactoring.com>
2019-08-04 10:14:45 +1000

    Get resources from local instead of internet
    - Make the test more resilient

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,25 +1,34 @@
 import request from 'supertest'
 import fs from 'fs'
 import path from 'path'
+import nock from 'nock'
 const server = require('./server')
 
 describe('Image filter api', () => {
-    it('returns the image from the url with the filter applied', async () => {
-        await request(server)
-            .get('/filteredimage?image_url=https://timedotcom.files.wordpress.com/2019/03/kitten-report.jpg')
-            .expect(200)
-            .then(response => {
-                expect(response.header['content-type']).toBe('image/jpeg')
+    describe('filter image', () => {
+
+        beforeEach(() => {
+            nock('http://localhost')
+                .get('/kitten.jpg')
+                .reply(200,
+                    fs.readFileSync(path.join(__dirname, '/testdata/kitten.jpg')))
+        })
+        it('returns the image from the url with the filter applied', async () => {
+            const response = await request(server)
+                .get('/filteredimage?image_url=http://localhost/kitten.jpg')
+                .expect(200)
+
+            expect(response.header['content-type']).toBe('image/jpeg')
+        })
+
+        it('deletes the temporary image file after sending it', async () => {
+            await request(server)
+                .get('/filteredimage?image_url=http://localhost/kitten.jpg')
+                .expect(200)
+
+            fs.readdir(path.join(__dirname, '/util/tmp/'), (_, files) => {
+                expect(files.length).toBe(0)
             })
-    })
-
-    it('deletes the temporary image file after sending it', async () => {
-        await request(server)
-            .get('/filteredimage?image_url=https://timedotcom.files.wordpress.com/2019/03/kitten-report.jpg')
-            .expect(200)
-
-        fs.readdir(path.join(__dirname, '/util/tmp/'), (_, files) => {
-            expect(files.length).toBe(0)
         })
     })
 
@@ -32,9 +41,13 @@ describe('Image filter api', () => {
     })
 
     it('returns an error when the image_url parameter is not an image', async () => {
+        nock('http://localhost')
+            .get('/')
+            .reply(200, `I'm not an image url`)
+
         const response = await request(server)
-        .get('/filteredimage?image_url=http://www.google.com')
-        .expect(422)
+            .get('/filteredimage?image_url=http://localhost')
+            .expect(422)
 
         expect(response.body).toEqual({ error: 'image_url is not an image' })
     })
